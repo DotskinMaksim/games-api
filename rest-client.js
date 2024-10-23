1fd86064a62eb5e6680f388d8aafa96dd234e9cc
DotskinMaksim <maxell9976@gmail.com>
2024-10-23 11:53:11 +0300

    As a user I can see details of the game [Delivers #188467534]

--- rest-client.js
+++ rest-client.js
@@ -1,14 +1,15 @@
 const app = Vue.createApp({
     data() {
         return {
+            gameInModal: { id: null, name: null, price: null },
             games: [],
             sortKey: '',
             sortAsc: true
         };
     },
     methods: {
-        sortBy(key) {
 
+        sortBy(key) {
             if (this.sortKey === key) {
                 this.sortAsc = !this.sortAsc;
             } else {
@@ -16,11 +17,17 @@ const app = Vue.createApp({
                 this.sortAsc = true;
             }
 
+
             this.games.sort((a, b) => {
                 if (a[key] < b[key]) return this.sortAsc ? -1 : 1;
                 if (a[key] > b[key]) return this.sortAsc ? 1 : -1;
                 return 0;
             });
+        },
+        async getGame(id) {
+            this.gameInModal = await (await fetch(`http://localhost:8080/games/${id}`)).json();
+            let gameInfoModal = new bootstrap.Modal(document.getElementById('gameInModal'));
+            gameInfoModal.show();
         }
     },
     async created() {
